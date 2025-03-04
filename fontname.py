2754ee263839f5968ac2b57b8d86a390e63a7b0c
Cornelia Lin <cornelia.lin@tngtech.com>
2025-03-04 13:23:35 +0100

    Rewrite as uv script

--- fontname.py
+++ fontname.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S uv --quiet run --script
+
+# /// script
+# requires-python = ">3.6"
+# dependencies = [
+#     "fonttools"
+# ]
+# ///
 
 # ==========================================================================
 # fontname.py
